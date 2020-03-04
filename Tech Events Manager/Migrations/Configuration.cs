4e8714b6cfd14a298a2550a967a76c634896af36
Mike <michael-wilkins@sky.com>
2020-03-04 21:00:57 +0000

    Add project files.

---
+++ Tech Events Manager/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Tech_Events_Manager.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Tech_Events_Manager.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Tech_Events_Manager.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Tech Events Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tech_Events_Manager.Startup))]
+namespace Tech_Events_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
