6c2390a776aa3af7ea9fb9797ceddbc7e0a5ce00
Mike <michael-wilkins@sky.com>
2020-03-05 14:56:16 +0000

    Added model identity and migration to SQL Express

---
+++ Tech Events Manager/Models/EventConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Events_Manager.Models
+{
+    public class EventConstants
+    {
+        public const int MAX_ORGANISER_NAME_LENGTH = 50;
+        public const int MAX_CITY_LENGTH = 50;
+        public const int MAX_POSTCODE_LENGTH = 8;
+        public const int MIN_POSTCODE_LENGTH = 5;
+        public const int MAX_IMAGE_TITLE_LENGTH = 30;
+        public const int MAX_IMAGE_PATH_LENGTH = 50;
+      
+    }
+}
