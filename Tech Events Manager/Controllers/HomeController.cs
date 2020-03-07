4f47656b43836de3987b5969056539d32abff892
Mike <michael-wilkins@sky.com>
2020-03-07 13:38:45 +0000

    Updated views for admin and home page.

--- Tech Events Manager/Controllers/HomeController.cs
+++ Tech Events Manager/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tech_Events_Manager.Models;
 
 namespace Tech_Events_Manager.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Events
         public ActionResult Index()
         {
-            return View();
+            return View(db.Event.ToList());
         }
+
+     /*   public ActionResult Index()
+        {
+            return View();
+        }*/
 
         public ActionResult About()
         {
