7d67ab631e126fd25c7f278665c0975cad9b1049
Mike <michael-wilkins@sky.com>
2020-03-20 11:40:40 +0000

    Created server side validation

--- Tech Events Manager/ViewModel/CustomerViewModel.cs
+++ Tech Events Manager/ViewModel/CustomerViewModel.cs
@@ -13,7 +13,11 @@ namespace Tech_Events_Manager.ViewModel
 {
     public class CustomerViewModel
     {
-        [Required(ErrorMessage = "Enter a valid postcode")]
+       
+        [Required(ErrorMessage = "Enter a valid UK postcode")]
+       // [RegularExpression("(^gir\\s?0aa$)|(^[a-z-[qvx]](\\d{1,2}|[a-hk-y]\\d{1,2}|\\d[a-hjks-uw]|[a-hk-y]\\d[abehmnprv-y])\\s?\\d[a-z-[cikmov]]{2}$)", ErrorMessage = "Postcode must be UK")]
+        [RegularExpression("^([A-Z]{1,2})([0-9][0-9A-Z]?) ([0-9])([ABDEFGHJLNPQRSTUWXYZ]{2})$", ErrorMessage ="Uk Postcode")]
+        
         public string UserPostcode { get; set; }
         public double Distance { get; set; }
 
@@ -21,5 +25,9 @@ namespace Tech_Events_Manager.ViewModel
         public double UserLng { get; set; }
         public IEnumerable<Event> Event { get; set; }
 
+      
+       
     }
+   
+ 
 }
